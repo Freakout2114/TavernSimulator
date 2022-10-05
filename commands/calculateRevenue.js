124b577d19d3375303a08c99cc022b04fe467b81
Harry Barden <harry.barden@hotmail.com>
2022-10-05 18:44:42 +1100

    Updated how revenue is calculated

--- commands/calculateRevenue.js
+++ commands/calculateRevenue.js
@@ -1,22 +1,11 @@
-/**
- * Once a tenday, the party should roll 2d20 - 10 to determine the profit percentage (range -8% to +30%).
- *
- * To calculate profits earned, multiply the percentage by the cost found on the chart below.
- *
- * Valuation    Tavern Quality      Expected Cost Per Tenday
- * 1-5          Squalid             45 gp
- * 6-12         Poor                60 gp
- * 13-19        Modest              75 gp
- * 20-30        Comfortable         95 gp
- * 31-44        Upscale             125 gp
- * 45+          Aristocratic        160 gp
- */
-
 const { SlashCommandBuilder } = require('discord.js');
 const Tavern = require('../src/tavern.js');
 
 const calculate = function() {
-    return new Tavern().calculateRevenue();
+    const tavern = new Tavern();
+    const revenueObj = tavern.calculateRevenue();
+    const overview = tavern.getOverview(revenueObj);
+    return overview;
 }
 
  module.exports = {
@@ -24,7 +13,7 @@ const calculate = function() {
         .setName('calculaterevenue')
         .setDescription('Calculate the revenue for the tavern.'),
     async execute(interaction) {
-        const profit = calculate();
-        await interaction.reply('Revenue: ' + profit);
+        const result = calculate();
+        await interaction.reply(result);
     },
 };
--- src/tavern.js
+++ src/tavern.js
@@ -4,6 +4,22 @@ module.exports = function() {
     this.valuation = 1;
     this.employeeCount = 8;
 
+    this.getOverview = function(revenueObj) {
+        const {
+            baseProfit,
+            employeeWages,
+            upgrades,
+            profit
+        } = revenueObj;
+
+        return `**Revenue**: ${profit.toString()}\n` +
+            `\n` +
+            `**Breakdown**\n` +
+            `Base Profit: ${baseProfit.toString()}\n` +
+            `Employee Wages: -${employeeWages.toString()}\n` +
+            `Upgrade Bonuses: ${upgrades.toString()}`;
+    }
+
     this.calculateRevenue = function() {
         const expectedCost = this.getExpectedCostPerTenday();
         const profitPercentage = (Math.random() * 38 - 8) / 100.0; // Range from -8% to +30%
@@ -21,12 +37,19 @@ module.exports = function() {
 
         // If one or more characters spend a full Downtime managing tavern operations, they may add 1d4 to the profit percentage roll.
 
-        const baseProfit = expectedCost.mult(profitPercentage).value();
-        const employeeWages = employeeWage.mult(employeeCount).value();
-        const upgrades = new Currency().value();
-        const profit = new Currency().copper(baseProfit - employeeWages + upgrades).toGold().toString();
+        const baseProfit = expectedCost.mult(profitPercentage);
+        const employeeWages = employeeWage.mult(employeeCount);
+        const upgrades = new Currency();
 
-        return profit;
+        const profit = new Currency().copper(baseProfit.value() - employeeWages.value() + upgrades.value());
+
+        return {
+            baseProfit,
+            employeeWages,
+            upgrades,
+            profit
+        };
+
     }
 
     this.getQuality = function() {
@@ -46,6 +69,19 @@ module.exports = function() {
         }
     }
 
+    /**
+     * Once a tenday, the party should roll 2d20 - 10 to determine the profit percentage (range -8% to +30%).
+     *
+     * To calculate profits earned, multiply the percentage by the cost found on the chart below.
+     *
+     * Valuation    Tavern Quality      Expected Cost Per Tenday
+     * 1-5          Squalid             45 gp
+     * 6-12         Poor                60 gp
+     * 13-19        Modest              75 gp
+     * 20-30        Comfortable         95 gp
+     * 31-44        Upscale             125 gp
+     * 45+          Aristocratic        160 gp
+     */
     this.getExpectedCostPerTenday = function() {
         const valuation = this.valuation;
         if (valuation <= 5) {
