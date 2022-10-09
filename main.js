f49f69626e09bd7d01d5420922ce72e50fd1a613
Harry Barden <harry.barden@hotmail.com>
2022-10-09 16:33:28 +1100

    Updated seed function

--- main.js
+++ main.js
@@ -41,6 +41,24 @@ client.login(token);
 
 // --- Other stuff ---
 
+// Hashing functions
+function cyrb128(str) {
+    let h1 = 1779033703, h2 = 3144134277,
+        h3 = 1013904242, h4 = 2773480762;
+    for (let i = 0, k; i < str.length; i++) {
+        k = str.charCodeAt(i);
+        h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+        h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+        h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+        h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+    }
+    h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+    h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+    h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+    h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+    return [(h1^h2^h3^h4)>>>0, (h2^h1)>>>0, (h3^h1)>>>0, (h4^h1)>>>0];
+}
+
 // Change the Math.random function to use a seed
 function mulberry32(a) {
     return function() {
@@ -51,7 +69,10 @@ function mulberry32(a) {
     }
 }
 // The seed is the current date e.g. yyyymmdd
-const seed = new Date().toISOString()
+const seedInput = new Date().toISOString()
 	.split('T')[0]
-	.replace('-','');
-Math.random = mulberry32(seed);
+	.replaceAll('-','');
+const seed = cyrb128(seedInput);
+
+// Set the random function to be seeded
+Math.random = mulberry32(seed[0]);
